b3013a3195004308f6820180ed6487fb28b3c9ad
sora <ksora093@gmail.com>
2025-06-06 14:02:28 +0900

    [fix] ダークモード切替スイッチの挙動を修正
    アイコンをlabelタグからdivタグに変更し、アイコン自体に操作を持たせないよう修正

--- src/components/todo/DarkModeSwitch.tsx
+++ src/components/todo/DarkModeSwitch.tsx
@@ -1,5 +1,4 @@
 import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { Label } from "../ui/label";
 import { Switch } from "../ui/switch";
 
 interface DarkModeSwitchProps {
@@ -14,17 +13,16 @@ export default function DarkModeSwitch({
   return (
     <div className="flex items-center relative">
       <Switch
-        id="dark-mode"
         defaultChecked={isDarkMode}
         onCheckedChange={() => setIsDarkMode(!isDarkMode)}
         className="scale-200 ml-4 h-[18px]"
       />
-      {!isDarkMode && (<Label htmlFor="dark-mode" className="text-lg absolute left-9.5">
+      {!isDarkMode && (<div className="text-lg absolute left-9.5">
         <MdDarkMode />
-      </Label>)}
-      {isDarkMode && (<Label htmlFor="dark-mode" className="text-lg absolute left-2">
+      </div>)}
+      {isDarkMode && (<div className="text-lg absolute left-2">
         <MdLightMode className="text-white"/>
-      </Label>)}
+      </div>)}
     </div>
   );
 }
